e017b2f40ff6c03cfaca5bc6309fdfaa80399b32
rub1nJr <rubens.cjnr@gmail.com>
2021-06-23 19:06:45 -0300

    Update ex011.js

--- exercicios/ex010/ex011.js
+++ exercicios/ex010/ex011.js
@@ -1,6 +1,9 @@
-var idade = 17
-if(idade < 18){
-    console.log(`Menor de idade`)
-} else {
-    console.log(`Maior de idade`)
+var idade = 20
+console.log(`Você tem ${idade} anos!`)
+if(idade < 16){
+    console.log(`Não vota!`)
+} else if(idade < 18 || idade >65){
+    console.log(`Voto opcional!`)
+} else{
+    console.log(`Voto obrigatorio!!!`)
 }
