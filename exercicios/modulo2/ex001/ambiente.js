2c748a13767b2478fd3f2a142f053c6158e4b68e
rub1nJr <rubens.cjnr@gmail.com>
2021-06-23 20:19:43 -0300

    Update ambiente.js

--- exercicios/modulo2/ex001/ambiente.js
+++ exercicios/modulo2/ex001/ambiente.js
@@ -1,9 +1,18 @@
 //testando loop
 
 var c = 1
+do {
+    console.log(`Passo ${c}!`)
+    c++ 
+} while(c <= 10)
+
+
+
+/*
+var c = 1
 while(c <= 20){
     console.log(`Passo ${c}!`)
     c++ // c = c+1
 }
+*/
 
-//
