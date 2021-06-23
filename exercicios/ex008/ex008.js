4be4cd79323c17903e03a2d07729568a43d23785
rub1nJr <rubens.cjnr@gmail.com>
2021-06-22 22:24:14 -0300

    Update ex008.js

--- exercicios/ex008/ex008.js
+++ exercicios/ex008/ex008.js
@@ -1,9 +1,9 @@
 
 
-var vel = 60.5
+var vel = 58
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
 
-    if(vel > 60){
+    if(vel > 60){ //consição simples
         console.log('Você ultrapassou a velocidade permitida. MULTADO!!')
 }
 console.log('Dirija sempre usando sinto de segurança')
