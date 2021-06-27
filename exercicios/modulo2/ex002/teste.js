73f34e7b0e6ba8abbb3d8e7d9662c44dc2dd16f9
rub1nJr <rubens.cjnr@gmail.com>
2021-06-27 17:18:15 -0300

    Update teste.js

--- exercicios/modulo2/ex002/teste.js
+++ exercicios/modulo2/ex002/teste.js
@@ -5,3 +5,16 @@ console.log(`A variavel possui ${val.length} Casas.`)
 for (let a = 0; a < val.length; a++){
     console.log(`A posição ${a} possui o valor ${val[a]}`)
 }
+
+console.log('DIVIDINDO')
+console.log('===============')
+console.log('DIVIDINDO')
+
+let c = [5,8,4,9,6]
+let d = c.indexOf(4)
+c.sort()
+    for(let b in c){
+        console.log(`A posição ${b} possui o valor ${c[b]}`)
+    }
+    let f = c.indexOf(4)
+    console.log(`O valor 4 esta na posição ${d} mas apos o (sort) ele fica na posição ${f}`)
