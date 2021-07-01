a4d5548396b8eb058423c1f2dd01a3e344f1632d
rub1nJr <rubens.cjnr@gmail.com>
2021-06-30 22:37:55 -0300

    desafio 2.1

---
+++ desafios/desafio2/jscript.js
@@ -0,0 +1,22 @@
+function verificar(){
+
+    var data = new Date()
+    var ano = data.getFullYear()
+    var infAno = document.querySelector('input#txtnc')
+    var anoNascimento = Number(infAno.value)
+    var idade = ano - anoNascimento
+    var res = document.querySelector('#result')
+
+    if (anoNascimento == 0 || anoNascimento > ano) {
+        window.alert('ERRO! Verifique os dados e tente novamente!')
+    } else {
+        if {
+
+        }
+
+    }
+
+
+}
+
+
