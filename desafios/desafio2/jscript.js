1b9669b8c6dc1787b2b5d85674bb421c65698560
rub1nJr <rubens.cjnr@gmail.com>
2021-07-01 22:13:44 -0300

    Update jscript.js

--- desafios/desafio2/jscript.js
+++ desafios/desafio2/jscript.js
@@ -2,47 +2,45 @@ function verificar(){
 
     var data = new Date()
     var ano = data.getFullYear()
-    var infAno = document.querySelector('input#txtnc')
-    var anoNascimento = Number(infAno.value)
-    var idade = ano - anoNascimento
+    var fano = document.querySelector('#txtnc')
     var res = document.querySelector('#result')
-    var img = document.querySelector('#imgS')
+    var anoNascimento = Number(fano.value)
+    var idade = ano - anoNascimento
 
     if (anoNascimento == 0 || anoNascimento > ano) {
-        window.alert('ERRO! Verifique os dados e tente novamente!')
+            window.alert('ERRO! Verifique os dados!')
     } else {
-       var fsex = document.getElementsByName('sexrd')
-       var genero = ''
-    } if (fsex[0].checked) {
-        genero = 'Homem'
-        if ( idade <= 10){
-            img.src='imagens/crianca-m.jpg'
-        } else if ( idade > 10 && idade < 18 ) {
-            img.src='imagens/adolescente-m.jpg'
-        } else if (idade >= 18 && idade <= 30){
-            img.src='imagens/m-adulto-jovem.jpg'
-        } else if (idade > 30 && idade < 60 ){
-            img.src='imagens/adulto-m.jpg'
-        } else {
-            img.src='imagens/idoso-m.jpg'
+        var img = document.querySelector('#imgS')
+        var fsex = document.getElementsByName('sexrd')
+        var genero = ''
+        if (fsex[0].checked){
+            if (idade <= 10){
+                img.src='imagens/crianca-m.jpg'
+            } else if ( idade > 10 && idade < 18){
+                img.src='imagens/adolescente-m.jpg'
+            } else if ( idade >= 18 && idade < 30 ) {
+                img.src='imagens/m-adulto-jovem.jpg'
+            } else if ( idade > 30 && idade <= 60 ){
+                img.src='imagens/adulto-m.jpg'
+            } else if (idade > 60 ){
+                img.src='imagens/idoso-m.jpg'
+            }
+        
+        } else if (fsex[1].checked){
+            if (idade <= 10){
+                img.src='imagens/crianca-f.jpg'
+            } else if ( idade > 10 && idade < 18){
+                img.src='imagens/adolescente-f.jpg'
+            } else if ( idade >= 18 && idade < 30 ) {
+                img.src='imagens/f-adulto-jovem.jpg'
+            } else if ( idade > 30 && idade <= 60 ){
+                img.src='imagens/adulto-f.jpg'
+            } else if (idade > 60 ){
+                img.src='imagens/idosa-f.jpg'
+            }
         }
-
-    } else if (fsex[1].checked){
-        genero = 'Mulher'
-        if ( idade <= 10){
-            img.src='imagens/crianca-f.jpg'
-        } else if ( idade > 10 && idade < 18 ) {
-            img.src='imagens/adolescente-f.jpg'
-        } else if (idade >= 18 && idade <= 30){
-            img.src='imagens/f-adulto-jovem.jpg'
-        } else if (idade > 30 && idade < 60 ){
-            img.src='imagens/adulto-f.jpg'
-        } else {
-            img.src='imagens/idosa-f.jpg'
     }
+    res.innerHTML = `Detectamos um(a) ${genero} de ${idade} anos!`
+
+
 }
-    res.innerHTML = `Detectamos ${genero} de ${idade} anos !`
-
-}
-
-
