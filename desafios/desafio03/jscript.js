f5068a8a013db5b516973d08dc20f5bd7ee27d1e
rub1nJr <rubens.cjnr@gmail.com>
2021-07-03 08:49:05 -0300

    Create jscript.js

---
+++ desafios/desafio03/jscript.js
@@ -0,0 +1,19 @@
+    function contar(){
+
+    var inicio = document.querySelector('#numeroInicio')
+    var final = document.querySelector('#numeroFinal')
+    var passo = document.querySelector('#numeroPassos')
+    var resultado = document.querySelector('#resultado')
+    var numInicio = Number(inicio.value)
+    var numFinal = Number(final.value)
+    var numPasso = Number(passo.value)
+    var num = 0
+
+        if (numInicio == 0 || numFinal == 0 || numPasso == 0){
+                window.alert('ERRO! Verifique os dados!')
+            
+        } else {
+
+        }
+
+    }
