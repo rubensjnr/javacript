0e1ae2670b14cc3239796a3795c5ff0a7b738358
rub1nJr <rubens.cjnr@gmail.com>
2021-07-04 12:33:14 -0300

    Update jscript.js

--- desafios/desafio03/jscript.js
+++ desafios/desafio03/jscript.js
@@ -1,10 +1,25 @@
 function contar(){
 
     var inicio = document.querySelector('#numeroInicio')
-    var fim = document.querySelector('#numeroFinal')
+    var final = document.querySelector('#numeroFinal')
     var passos = document.querySelector('#numeroPassos')
     var res = document.querySelector('#resultado')
+    var ini = Number(inicio.value)
+    var fim = Number(final.value)
+    var npassos = Number(passos.value)
 
-    
-
+    if (ini == 0 & fim == 0 || npassos == 0){
+        window.alert('ERRO! VERIFIQUE OS DADOS!!!')
+    } else {
+        res.innerHTML = `Contando: `
+        if (ini <= fim){
+            for ( let c = ini ; c <= fim ; c += npassos ){
+                res.innerHTML += `${c} \u{1F449}`
+            } 
+        } else {
+            for (let d = ini; d >= fim; d -= npassos){
+                res.innerHTML += `\u{1F449} ${d}`
+            }
+        }
+    }
 }
